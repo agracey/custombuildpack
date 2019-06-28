9c5b66775cc4f2ad4fa6925ca23e04e097d45e14
Andrew Gracey <agracey@gmail.com>
2019-06-27 17:14:40 -0700

    moving to FaaS

---
+++ app.js
@@ -0,0 +1,25 @@
+import http from 'http'
+
+import func from './function.js'
+
+
+const addContentType = (a) => (Object.assign({}, a, {"Content-Type": "application/json"}))
+
+const write = (res, data, code, headers) => {
+  res.writeHead(code, addContentType(headers))
+  res.end(JSON.stringify(data))
+}
+
+
+http.createServer((req,res)=>{
+  console.log('Recieved Request')
+  func(req).then((data, headers)=>{
+    const code = !data ? 204 : 200
+    write(res, data, code, headers)
+  }).catch((err, code, headers)=>{
+    write(res, err | {error: 'Unknown Error'}, code | 500, headers)
+  })
+}).listen(process.env.PORT || 8080)
+
+
+console.log("Server Listening on ", process.env.PORT || 8080)
