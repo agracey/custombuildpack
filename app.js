d2778a715e51f88e4556c1a7427d8f3d85bf293c
Andrew Gracey <agracey@gmail.com>
2019-07-16 13:48:22 -0700

    fixing typo in redirect

--- app.js
+++ app.js
@@ -4,39 +4,70 @@ const func = require('./function.js')
 
 const addContentType = (a) => (Object.assign({}, a, {"Content-Type": "application/json"}))
 
-const write = (res, data, code, headers) => {
+const write = (res, data, code, headers={}) => {
   res.writeHead(code, addContentType(headers))
   res.end(JSON.stringify(data))
 }
 
-const getData = (req) =>{
-  return new Promise(function(resolve){
-    //console.log("REQ:", JSON.stringify(req,params))
+let vcap = null
+const getVCap = () =>{
+  if(!vcap) vcap = JSON.parse(process.env.VCAP_SERVICES)
+  return vcap
+}
+
+const buildContext = async (req) => {
+  const services = getVCap()
+  const user = req.headers.Authentication
+  return {services, user}
+}
+
+const buildEvent = (req) => {
+  return new Promise(function(resolve, reject){
 
     var data = '';
-    req.on('data', function( chunk ) {
+    req.on('data', ( chunk ) => {
       data += chunk;
-    });
+    })
 
-    req.on('end', function() {
+    req.on('end', () => {
       req.rawBody = data;
       if (data && data.indexOf('{') > -1 ) {
         req.body = JSON.parse(data);
       }
-      resolve({body:req.body})
-    });
+      resolve({data:req.body})
+    })
+
+    req.on('error', (err)=>{
+      reject(err)
+    })
   })
 }
+
+
+const handleRequest = async (req) => {
+  const event = await buildEvent(req) 
+  const context = await buildContext(req)
+  const data = await func.run(event,context)
+  return {data}
+}
+
+
+
+
+
+
+// Allow for attaching an init function for database connection etc...
+if (func.init) func.init(getVCap())
 
 http.createServer((req,res)=>{
   console.log('Recieved Request')
 
-  getData(req).then(func).then((data, headers)=>{
+  handleRequest(req).then(({data, headers})=>{
     const code = !data ? 204 : 200
     write(res, data, code, headers)
-  }).catch((err, code, headers)=>{
-    console.log('ERROR',err)
-    write(res, err || {error: 'Unknown Error'}, code || 500, headers)
+  }).catch((err)=>{
+    console.log('ERROR: ',err)
+    write(res, err || {error: 'Unknown Error'}, 500, headers)
   })
 
 }).listen(process.env.PORT || 8080)
